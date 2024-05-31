96fe103a136159833dfd9da5a90c0f6bc6f44b38
Rifqi Muzhaffar <rifqi.city@gmail.com>
2024-05-31 19:11:45 +0700

    day 2

--- 2-Find-the-smallest-integer-in-the-array.js
+++ 2-Find-the-smallest-integer-in-the-array.js
@@ -1,3 +1,17 @@
+// CODEWARSCHALLENGE 2024
+// DAY 2/366
+// Find the smallest integer in the array
+// https://www.codewars.com/kata/55a2d7ebe362935a210000b2
+
+// DESCRIPTION:
+// Given an array of integers your solution should find the smallest integer.
+
+// For example:
+
+// Given [34, 15, 88, 2] your solution will return 2
+// Given [34, -345, -1, 100] your solution will return -345
+// You can assume, for the purpose of this kata, that the supplied array will not be empty.
+
 function findSmallestInt(arr) {
   return Math.min(...arr);
 }
