9936080d3a60a63eca153b96e2cbc40534181bfa
Rifqi Muzhaffar <rifqi.city@gmail.com>
2024-06-02 15:14:57 +0700

    day 4

---
+++ 4-Beginner-Lost-Without-a-Map.js
@@ -0,0 +1,15 @@
+// CODEWARSCHALLENGE 2024
+// DAY 4/366
+// Beginner - Lost Without a Map
+// https://www.codewars.com/kata/57f781872e3d8ca2a000007e
+
+// DESCRIPTION:
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+const maps = (x) => x.map((x) => x * 2);
+
+console.log(maps([4, 1, 1, 1, 4]));
