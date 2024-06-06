335cb457ec2d0a0845c1a3076cb390a8ff10bd10
Rifqi Muzhaffar <rifqi.city@gmail.com>
2024-06-06 18:20:59 +0700

    -

---
+++ day7.js
@@ -0,0 +1,13 @@
+// CODEWARSCHALLENGE 2024
+// DAY 7/366
+// You Can't Code Under Pressure #1
+// https://www.codewars.com/kata/53ee5429ba190077850011d4
+
+// DESCRIPTION:
+// Code as fast as you can! You need to double the integer and return it.
+
+function doubleInteger(i) {
+  return i * 2;
+}
+
+console.log(doubleInteger(2));
