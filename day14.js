fef2a0df006b67ace452a14e1001a9db24653d84
Rifqi Muzhaffar <rifqi.city@gmail.com>
2024-06-15 12:28:25 +0700

    add description

--- day14.js
+++ day14.js
@@ -1,7 +1,11 @@
 // CODEWARSCHALLENGE 2024
 // DAY 14/366
-// Remove String Spaces
-// https://www.codewars.com/kata/57eae20f5500ad98e50002c5
+// Convert a String to a Number!
+// https://www.codewars.com/kata/544675c6f971f7399a000e79
+
+// Description
+// We need a function that can transform a string into a number. What ways of achieving this do you know?
+// Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
 
 // cara 1
 const stringToNumber = function (str) {
