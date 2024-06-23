c2edc1fda39c95855b548e2cadc281fca0536e84
Rifqi Muzhaffar <rifqi.city@gmail.com>
2024-06-23 23:09:00 +0700

    add description

--- Day 21-40/day21.js
+++ Day 21-40/day21.js
@@ -1,3 +1,12 @@
+// CODEWARSCHALLENGE 2024
+// DAY 21/366
+// Keep Hydrated!
+// https://www.codewars.com/kata/582cb0224e56e068d800003c
+
+// Nathan loves cycling.
+// Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+// You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+
 function litres(time) {
   return Math.floor(time * 0.5);
 }
