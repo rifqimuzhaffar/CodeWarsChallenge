ed016cbf88631233ad4b4534575d201402edc03a
Rifqi Muzhaffar <rifqi.city@gmail.com>
2024-06-25 11:45:44 +0700

    day 22

---
+++ Day 21-40/day22.js
@@ -0,0 +1,8 @@
+// CODEWARSCHALLENGE 2024
+// DAY 22/366
+// Convert a Boolean to a String
+// https://www.codewars.com/kata/551b4501ac0447318f0009cd
+
+const booleanToString = (b) => String(b);
+
+console.log(booleanToString(true));
