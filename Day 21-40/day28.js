0998048392fa042e2cab87d54c18eb39f268a848
Rifqi Muzhaffar <rifqi.city@gmail.com>
2024-07-02 20:35:08 +0700

    day 28

---
+++ Day 21-40/day28.js
@@ -0,0 +1,9 @@
+// CODEWARSCHALLENGE 2024
+// DAY 28/366
+// MakeUpperCase
+// https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7
+
+// Write a function which converts the input string to uppercase.
+const makeUpperCase = (str) => str.toUpperCase();
+
+console.log(makeUpperCase("hello"));
