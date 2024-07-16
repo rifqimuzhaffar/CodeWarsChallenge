715ea9f7ac47db7e603884136ddb1877530cb8cb
Rifqi Muzhaffar <rifqi.city@gmail.com>
2024-07-16 16:37:03 +0700

    day36

---
+++ Day 21-40/day36.js
@@ -0,0 +1,10 @@
+// CODEWARSCHALLENGE 2024
+// DAY 36/366
+// Convert a string to an array
+// https://www.codewars.com/kata/57e76bc428d6fbc2d500036d
+
+function stringToArray(string) {
+  return string.split(" ");
+}
+
+console.log(stringToArray("Robin Singh"));
