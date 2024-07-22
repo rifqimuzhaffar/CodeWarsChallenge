eb7fc6783d25b0acf3128f41e8d0a2d089e52d71
Rifqi Muzhaffar <rifqi.city@gmail.com>
2024-07-22 20:26:46 +0700

    day47

---
+++ Day 41-60/day47.js
@@ -0,0 +1,14 @@
+// CODEWARSCHALLENGE 2024
+// DAY 47/366
+// Jenny's secret message
+// https://www.codewars.com/kata/55225023e1be1ec8bc000390
+
+// DESCRIPTION :
+// Jenny has written a function that returns a greeting for a user. However, she's in love with Johnny, and would like to greet him slightly different. She added a special case to her function, but she made a mistake.
+// Can you help her?
+
+function greet(name) {
+  return name === "Johnny" ? "Hello, my love!" : "Hello, " + name + "!";
+}
+
+console.log(greet("Johnny"));
