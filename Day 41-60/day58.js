a8a310593b859fe34f9bf93b0114dbf482e7277e
Rifqi Muzhaffar <rifqi.city@gmail.com>
2024-09-28 11:28:22 +0700

    day 58

---
+++ Day 41-60/day58.js
@@ -0,0 +1,13 @@
+// CODEWARSCHALLENGE 2024
+// DAY 58/366
+// Grasshopper - Combine strings
+// https://www.codewars.com/kata/55f73f66d160f1f1db000059
+
+function combineNames(a, b) {
+  return a + " " + b;
+}
+
+// simple code
+// const combineNames = (...names) => names.join(' ');
+
+console.log(combineNames("James", "Stevens"));
